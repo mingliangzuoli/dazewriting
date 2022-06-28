132a8110884ae04783775902389fece77e01508c
GitHub Action <action@github.com>
2022-06-28 08:52:20 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/mingliangzuoli/dazewriting/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mingliangzuoli.dazewriting.services
+
+import com.github.mingliangzuoli.dazewriting.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
